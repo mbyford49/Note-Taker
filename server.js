bc3424eb7d9799242c46fab3e77c65236eb5a625
mbyford49 <mbyford49@gmail.com>
2024-06-10 15:55:30 -0700

    write server.js file & created other necessary files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const apiRoute = require('./routes/apiRoute');
+const htmlRoute = require('./routes/htmlRoute');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
